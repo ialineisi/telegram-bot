58f470fd7315dfab3942730640124ce0545d5481
Ali <82692859+llBestBoyll@users.noreply.github.com>
2021-12-14 03:40:12 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-$input = file_get_contents('php://input');
-$update = json_decode($input);
-$message = $update->message;
-$chat_id = $message->chat->id;
-$text = $message->text;
 $token =
  '5023237329:AAGNIM7yMslc4SJFtzvI4r-vrr9DM5fGuac';
 file_get_contents(
 "https://api.telegram.org/
-bot$token/sendMessage?chat_id=$chat_id&text=$text");
+bot$token/sendMessage?chat_id=329949912&text=hey");
+?>
